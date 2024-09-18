e10bc3c705b6de923a24cc5d4217ef89908ec276
pythonesque13 <yvan.tsemegne@facsciences-uy1.cm>
2024-09-18 12:23:32 +0100

    une application pour une publicité

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import Background from "./Components/Background/Background";
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
+import { useEffect, useState } from "react";
+const App = () => {
+  let heroData=[
+    {text1:"Dive into", text2:"what you love"},
+    {text1:"Indulge", text2:"your passions"},
+    {text1:"Give in to", text2:"your passions"},
+  ]
+
+  const [heroCount, setHeroCount]= useState(0);
+  const [playStatus, setPlayStatus]= useState(false);
+
+  useEffect(()=>{
+    setInterval(() => {
+      setHeroCount((count)=>{return count===2?0:count+1})
+    }, 3000);
+  }, [])
+
+  return (
+    <div>
+      <Background playStatus={playStatus} heroCount={heroCount} />
+      <Navbar />
+      <Hero
+        setPlayStatus={setPlayStatus}
+        setHeroCount={setHeroCount}
+        heroCount={heroCount}
+        heroData={heroData[heroCount]}
+        playStatus={playStatus}
+      />
+    </div>
+  )
+}
+
+export default App
